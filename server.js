108391a50ccc37daaf503e7b4eb1dbdc1facd980
Ubuntu <ubuntu@ip-172-31-43-207.us-west-2.compute.internal>
2016-08-04 12:49:07 +0000

    server

---
+++ server.js
@@ -0,0 +1,8 @@
+express = require('express');
+var app = express();
+
+app.use('/', express.static(__dirname + '/'));
+
+app.listen(80, function () {
+  console.log('Example app listening on port 80!');
+});
