70ee4df1c7b9eb91c28c8f59cee6ee73c658e872
orikurt <orikball@gmail.com>
2016-08-04 17:02:48 +0300

    timer improvement

--- js/trillion.js
+++ js/trillion.js
@@ -1,5 +1,11 @@
 $(document).ready(function(){
+	update_tril();
 	setInterval(function(){
+		update_tril();
+	}, 60000);
+});
+
+var update_tril = function(){
 		var trilost = parseInt($("#trilost").html().replace("$", "").split(",").join(""));
 		var change = (Math.random() + 1) / 100;
 		var randBool = Math.random()<.5;
@@ -7,6 +13,5 @@ $(document).ready(function(){
 			change = change * -1;
 		}
 		trilost += change * trilost;
-		$("#trilost").html(numeral(trilost).format('$0,0'));
-	}, 60000);
-});
+		$("#trilost").html(numeral(trilost).format('$0,0'));	
+};
